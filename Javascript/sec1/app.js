08289a96fef30c503aa8ffe5bcb52af0d50945cb
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2020-04-22 21:08:21 +0530

    acha h

---
+++ Javascript/sec1/app.js
@@ -0,0 +1,27 @@
+//Log to console
+console.log("Hello");
+console.log(123);
+console.log(true);
+var greet="jj";
+console.log(greet);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+
+console.error("tu acha h ");
+
+// console.clear();
+console.warn("Arre arre");
+
+console.time("hello world");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+    console.log("achaa ji");
+console.timeEnd("hello world");
+
+console.clear();
